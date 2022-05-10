8d2b4738e1af2f4cf59e9ac2a49dda5cf49492c4
poncianodev <lucasbponciano@hotmail.com>
2022-05-09 23:15:27 -0300

    Projeto Finalizado

--- src/controller/Controllador.js
+++ src/controller/Controllador.js
@@ -3,7 +3,9 @@ import { bebes } from "../model/reborn.js";
 
 export const getIndex = async (req, res) => {
     try {
-        const listBebes = await bebes.findAll();
+        const listBebes = await bebes.findAll({
+            order: [["id", "ASC"]],
+        });
         res.render("index.ejs", { listBebes });
         console.log(listBebes);
     } catch (error) {
@@ -32,5 +34,92 @@ export const getDeletar = async (req, res) => {
 };
 
 export const getCriar = async (req, res) => {
-    res.render("criar.ejs");
+    res.render("criar.ejs", { toggle: false });
+};
+
+export const postCriar = async (req, res) => {
+    try {
+        const {
+            nome_kit,
+            peso,
+            sexo,
+            tamanho,
+            corpinho,
+            data_nascimento,
+            img,
+            iframe,
+        } = req.body;
+        if (!nome_kit) {
+            res.send("O nome é obrigatório.");
+        } else if (!peso) {
+            res.send("O peso é obrigatório.");
+        } else if (!tamanho) {
+            res.send("O tamanho é obrigatório.");
+        } else if (!corpinho) {
+            res.send("O corpinho é obrigatório.");
+        } else if (!data_nascimento) {
+            res.send("O nascimento é obrigatório.");
+        } else if (!img) {
+            res.send("O img é obrigatório.");
+        } else if (!iframe) {
+            res.send("O iframe é obrigatório.");
+        }
+        await bebes.create({
+            nome_kit,
+            peso,
+            sexo,
+            tamanho,
+            corpinho,
+            data_nascimento,
+            img,
+            iframe,
+        });
+        res.render("criar.ejs", {
+            toggle: true,
+        });
+    } catch (error) {
+        res.send(error.message);
+    }
+};
+
+export const getEditar = async (req, res) => {
+    try {
+        const bebesAtual = await bebes.findByPk(req.params.id);
+        res.render("editar.ejs", { bebesAtual });
+    } catch (error) {
+        res.send(error.message);
+    }
+};
+
+export const postEditar = async (req, res) => {
+    try {
+        const {
+            nome_kit,
+            peso,
+            sexo,
+            tamanho,
+            corpinho,
+            data_nascimento,
+            img,
+            iframe,
+        } = req.body;
+        await bebes.update(
+            {
+                nome_kit: nome_kit,
+                peso: peso,
+                sexo: sexo,
+                tamanho: tamanho,
+                corpinho: corpinho,
+                data_nascimento: data_nascimento,
+                img: img,
+                iframe: iframe,
+            },
+            {
+                where: { id: req.params.id },
+            },
+        );
+        res.redirect("/");
+    } catch (error) {
+        res.send(error.message);
+    }
 };
